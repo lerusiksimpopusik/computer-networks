6d8fc71b00466bb7ae2120908bbcb49eaa9b6815
lerusiksimpopulik <144207170+lerusiksimpopulik@users.noreply.github.com>
2025-02-10 07:37:36 +0700

    task1

--- 1/sem1.py
+++ 1/sem1.py
@@ -11,9 +11,9 @@ with open('sem1_output.csv', 'w', newline='', encoding='utf-8') as f:
         try:
             result = subprocess.check_output(f"ping {ip}").decode('oem')
             match = re.search(r'Среднее\s*=\s*(\d+)\s*мсек', result)
-            print(result)
+            # print(result)
             avg_rtt = match.group(1) if match else "N/A"
 
-            f.write(f"{ip}, {avg_rtt} ms\n")  # Запись результата в файл
+            f.write(f"{ip}, {avg_rtt}\n")
         except subprocess.CalledProcessError:
-            f.write(f"{ip}, Ping failed\n")  # Запись ошибки в файл
+            f.write(f"{ip}, Ping failed\n")
