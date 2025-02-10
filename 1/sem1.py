d570c51e4af2c9aa573ef617dcd742f5d8894817
lerusiksimpopulik <144207170+lerusiksimpopulik@users.noreply.github.com>
2025-02-10 07:33:44 +0700

    task1

---
+++ 1/sem1.py
@@ -0,0 +1,19 @@
+import subprocess
+import re
+
+ips = ['google.com', 'chatgpt.com', 'nsu.ru', 'ya.ru', 'gismeteo.ru',
+       'pypi.org', 'youtube.com', 'vk.com', 'ru.wikipedia.org', 'otvet.mail.ru']
+
+with open('sem1_output.csv', 'w', newline='', encoding='utf-8') as f:
+    f.write("ip, rtt (ms)\n")
+
+    for ip in ips:
+        try:
+            result = subprocess.check_output(f"ping {ip}").decode('oem')
+            match = re.search(r'Среднее\s*=\s*(\d+)\s*мсек', result)
+            print(result)
+            avg_rtt = match.group(1) if match else "N/A"
+
+            f.write(f"{ip}, {avg_rtt} ms\n")  # Запись результата в файл
+        except subprocess.CalledProcessError:
+            f.write(f"{ip}, Ping failed\n")  # Запись ошибки в файл
